63dd6d4898d36460e955466f7cdb5192524de6cd
Aldocool275 <121561781+Aldocool275@users.noreply.github.com>
2023-02-01 16:40:40 +0530

    Project 3.py
    Display All positive integers in a list

---
+++ Project 3.py
@@ -0,0 +1,9 @@
+my_list1=[12,-7,5,64,-14]
+for number in my_list1:
+    if number>0:
+        print(number)
+
+my_list2=[12,14,-95,3]
+for number in my_list2:
+    if number>0:
+        print(number)
